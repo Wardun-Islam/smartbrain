ee642122ab796aa5b296e8186f7ad82c40e82566
Wardun <wardunislamnoon@gmail.com>
2020-04-16 20:21:55 +0600

    Updates

---
+++ precache-manifest.facd07864bbebd5c241b52efa7c1dc74.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f0c209c33273964ddce6e6823469533d",
+    "url": "/smartbrain/index.html"
+  },
+  {
+    "revision": "afe54d2b8430c4f686ca",
+    "url": "/smartbrain/static/css/2.3b6f1bda.chunk.css"
+  },
+  {
+    "revision": "2051bb678cbb7e4549a3",
+    "url": "/smartbrain/static/css/main.fff6749d.chunk.css"
+  },
+  {
+    "revision": "afe54d2b8430c4f686ca",
+    "url": "/smartbrain/static/js/2.b8f16520.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/smartbrain/static/js/2.b8f16520.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2051bb678cbb7e4549a3",
+    "url": "/smartbrain/static/js/main.5e617cc8.chunk.js"
+  },
+  {
+    "revision": "be86346f47316a9fda85",
+    "url": "/smartbrain/static/js/runtime-main.bb56a74b.js"
+  }
+]);
